e6d1a4415f607f2aa344c5298b2f9b70495316b4
Sylvester Maison Amanyi <samanyi@teamapt.com>
2019-12-17 17:42:32 +0100

    tools-box

---
+++ tools_box/config/tools_box.py
@@ -0,0 +1,62 @@
+from __future__ import unicode_literals
+from frappe import _
+
+
+def get_data():
+    return [
+        {
+            "label": _("Human Resources"),
+            "items": [
+                {
+                    "type": "doctype",
+                    "is_query_report": False,
+                    "name": "Staff Requisition Form",
+                    "label": "Staff Requisition"
+                },
+                {
+                    "type": "doctype",
+                    "is_query_report": False,
+                    "name": "Staff Replacement Request Form",
+                    "label": "Staff Replacement"
+                },
+                {
+                    "type": "doctype",
+                    "is_query_report": False,
+                    "name": "Overtime Request",
+                    "label": "Overtime Request"
+                },
+                {
+                    "type": "doctype",
+                    "is_query_report": False,
+                    "name": "Overtime Sheet",
+                    "label": "Overtime Sheet"
+                },
+            ]
+        },{
+            "label": _("Buying"),
+            "items": [
+                {
+                    "type": "doctype",
+                    "is_query_report": False,
+                    "name": "Purchase Requisition",
+                    "label": "Purchase requisition"
+                },
+            ]
+        },{
+            "label": _("Selling"),
+            "items": [
+                {
+                    "type": "report",
+                    "is_query_report": True,
+                    "name": "Authority to Load",
+                    "label": "Authority to Load"
+                },
+                {
+                    "type": "report",
+                    "is_query_report": True,
+                    "name": "Call Log",
+                    "label": "Call Log"
+                },
+            ]
+        },
+    ]
