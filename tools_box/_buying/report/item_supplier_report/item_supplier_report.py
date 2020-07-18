74b75ce46ee86fcaa0345f2e6034d031df0bef7d
Maison Armani <masonarmani38@gmail.com>
2020-07-18 09:39:16 +0100

    hello world

--- tools_box/_buying/report/item_supplier_report/item_supplier_report.py
+++ tools_box/_buying/report/item_supplier_report/item_supplier_report.py
@@ -27,7 +27,6 @@ def execute(filters=None):
         condtions += " AND p.supplier = '%s'" % filters.get('supplier')
 
     for item in purchase_items:
-        frappe.errprint(item)
         sql = "SELECT c.item_code, c.item_name, p.supplier, c.rate, p.creation as last_update FROM " \
               "`tabPurchase Order` p INNER JOIN `tabPurchase Order Item` c ON (p.name = c.parent) WHERE (c.item_code = '%s')" \
               " %s ORDER BY p.creation DESC LIMIT 1" % (item.get('name'),condtions)
